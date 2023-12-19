0f45dabb08e8c19e0c5969c5f773ce1f54382d42
Murali Solayappan <Murali.Solayappan@Lumen.com>
2023-12-19 16:00:15 +0530

    written oauthclient and with some basic modules.

--- snow_cmdb/oauthclient.py
+++ snow_cmdb/oauthclient.py
@@ -16,14 +16,6 @@ from ansible.module_utils import to_text, to_bytes, to_native
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
-token_endpoint="https://login.microsoftonline.com/72b17115-9915-42c0-9f1b-4f98e5a4bcd2/oauth2/v2.0/token"
-client_id = "f5dc7ffd-9be5-407a-b093-56a579ae9d85"
-client_secret = "kOy8Q~jxyWde~AiNzYjdG_gZdc1ljO6ERDyzvcPw"
-api_key = "APPKEY759512020091415435175420415"
-api_secure_key = b"AKI030662020091415435175463219"
-scope="api://f5dc7ffd-9be5-407a-b093-56a579ae9d85/.default"
-access_token = None ;
-token_response={}
 
 #### logger ###
 #set logger config
