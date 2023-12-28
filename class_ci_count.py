4bb030f7994332ecc239eff2c74574796940cd20
Murali Solayappan <murali.solayappan@lumen.com>
2023-12-28 19:59:27 +0000

    class count script finished

--- class_ci_count.py
+++ class_ci_count.py
@@ -15,8 +15,8 @@ atexit.register(end_task);
 ##### logging configuration ###
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", 
-                    datefmt="%Y-%m-%d-%H:%M:%S"
-                    level=logging.DEBUG
+                    datefmt="%Y-%m-%d-%H:%M:%S",
+                    level=logging.INFO
                     )
 
 #### initial configuration ###
@@ -25,19 +25,26 @@ vault_file          = base_dir + "/vault_lumen_snow"
 vault_password_file = base_dir + "/vault_password_file" ;
 
 cmdb_class_list =  list()
-with open("all_classes.txt") as clist:
+with open("all_classes.txt", "r") as clist:
     for cmdb_class in clist:
         cmdb_class_list.append(cmdb_class.strip())
 
+logging.info("{} classes found".format(len(cmdb_class_list)))
 
 credstore = CredentialsStoreVault(vault_file, vault_password_file)
 cred = credstore.get_credentials()
 auth = SnowApiAuth(cred)
 auth.refresh_token()
-snow_api = SnowCmdbApi('lumen', auth, page_limit=10, test_ci_count=10000000)
+snow_api = SnowCmdbApi('lumen', auth, page_limit=10, test_ci_count=20)
 
+class_count = dict()
 for cmdb_class in cmdb_class_list:
     clsurl = snow_api.get_cmdb_class_url(cmdb_class);
+    #logging.info("To fetch Class URL: {}".format(clsurl));
     ci_count = snow_api.get_class_ci_total_count(clsurl)
-    print("{} - {}".format(cmdb_class, ci_count))
-    time.sleep(5);
+    class_count[cmdb_class] = ci_count
+
+with open("ci_count.csv", "w") as cic:
+    cic.write("class_name\tci_count\n")
+    for k,v in sorted(class_count.items(), key=lambda item: item[1]):
+        cic.write("{}\t{}\n".format(k,v))
